4d58bc50853b0ae675bf6f2fb7bbe3d9eac1c90b
aicul313 <lucia_0313@naver.com>
2023-08-03 17:17:12 +0900

    Lv0. 문자열 출력하기

---
+++ Java/Lv0/_0803_1.java
@@ -0,0 +1,12 @@
+// 문자열 출력하기
+
+import java.util.*;
+
+public class _0803_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = sc.next();
+        sc.close();
+        System.out.println(result);
+    }
+}
