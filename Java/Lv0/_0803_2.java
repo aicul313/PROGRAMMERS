0fad3d9b6296918830ed9ffe6a56bddbc02da650
aicul313 <lucia_0313@naver.com>
2023-08-03 17:19:41 +0900

    Lv0. a와 b 출력하기

---
+++ Java/Lv0/_0803_2.java
@@ -0,0 +1,18 @@
+// a와 b 출력하기
+import java.util.*;
+
+public class _0803_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b = 0;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
